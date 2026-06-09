94bf12b370179cfd8a0df228c3a7662bb314f72e
BlazeOaktreesTang <blaze.oaktrees.tang@outlook.com>
2026-06-09 14:57:14 +0800

    Add files via upload

---
+++ OPCUAWindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(207, 57);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "连接";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(207, 102);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "断开";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(207, 147);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 40);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "读取";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(323, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "灯";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(621, 11);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(78, 25);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(592, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "a1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(592, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "a2";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(621, 40);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(78, 25);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(592, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "a3";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(621, 72);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(78, 25);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(592, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 15);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "a4";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(621, 102);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(78, 25);
+            this.textBox4.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(592, 141);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 15);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "a5";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(621, 131);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(78, 25);
+            this.textBox5.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(592, 170);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 15);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "a6";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(621, 160);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(78, 25);
+            this.textBox6.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(592, 199);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(23, 15);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "a7";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(621, 189);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(78, 25);
+            this.textBox7.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(592, 228);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(23, 15);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "a8";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(621, 218);
+            this.textBox8.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(78, 25);
+            this.textBox8.TabIndex = 18;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(592, 257);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(23, 15);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "a9";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(621, 247);
+            this.textBox9.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(78, 25);
+            this.textBox9.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 375);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox9;
+    }
+}
+
---
+++ OPCUAWindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,176 @@
+﻿using Opc.Ua;
+using Opc.Ua.Client;
+using OpcUaHelper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OPCUAHelper oPCUA;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string serverUrl = "opc.tcp://192.168.100.100:4840";
+            oPCUA = new OPCUAHelper();
+            oPCUA.OpenConnectOfAnonymous(serverUrl);
+            loop1();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            oPCUA.CloseConnect();
+        }
+        bool a1 = false;
+        int a2 = 0;
+        int a3 = 0;
+        float a4 = 0;
+        string a5 = "";
+        string a6 = "";
+        int a7 = 0;
+        int a8 = 0;
+        int a9 = 0;
+        int a10 = 0;
+        private string[] MonitorNodeTags = null;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //DataValue a21 = oPCUA.GetCurrentNodeValue ("ns=3;s=\"数据块_1\".\"Static_1\"");
+            //a1 = oPCUA.GetCurrentNodeValue<bool>("ns=3;s=\"数据块_1\".\"Static_1\"");
+            //a2 = oPCUA.GetCurrentNodeValue<short>("ns=3;s=\"数据块_1\".\"Static_2\"");
+            //a3 = oPCUA.GetCurrentNodeValue<int>("ns=3;s=\"数据块_1\".\"Static_3\"");
+            //a4 = oPCUA.GetCurrentNodeValue<float>("ns=3;s=\"数据块_1\".\"Static_4\"");
+            //a5 = oPCUA.GetCurrentNodeValue<string>("ns=3;s=\"数据块_1\".\"Static_5\"");//string
+            //a6 = oPCUA.GetCurrentNodeValue<string>("ns=3;s=\"数据块_1\".\"Static_6\"");//string
+            //a7 = oPCUA.GetCurrentNodeValue<int>("ns=3;s=\"OPC通讯数据块\".\"A1\"[3]");
+            //a8 = oPCUA.GetCurrentNodeValue<short>("ns=3;s=\"OPC通讯数据块\".\"c2\".\"A2\"");
+            //a9 = oPCUA.GetCurrentNodeValue<int>("ns=3;s=\"OPC通讯数据块\".\"c2\".\"A4\"[1]");
+            //a10 = oPCUA.GetCurrentNodeValue<int>("ns=3;s=\"数据块_1\".\"Static_7\"[1000]");//string
+
+            //// 添加所有的读取的节点，此处的示例是类型不一致的情况
+            //List<NodeId> nodeIds = new List<NodeId>();
+            //nodeIds.Add(new NodeId("ns=3;s=\"数据块_1\".\"Static_1\""));
+            //nodeIds.Add(new NodeId("ns=3;s=\"数据块_1\".\"Static_2\""));
+            //nodeIds.Add(new NodeId("ns=3;s=\"数据块_1\".\"Static_3\""));
+            //nodeIds.Add(new NodeId("ns=3;s=\"数据块_1\".\"Static_4\""));
+            //nodeIds.Add(new NodeId("ns=3;s=\"数据块_1\".\"Static_5\""));
+            //nodeIds.Add(new NodeId("ns=3;s=\"数据块_1\".\"Static_6\""));
+
+            //// 批量读
+            //Dictionary<string, DataValue> dataValues = oPCUA.GetBatchNodeDatasOfSync(nodeIds);
+
+
+            //单节点订阅  A是字典的Key，可以随便定义， 只用执行一次，值发生改变会调用 回调函数
+            oPCUA.SingleNodeIdDatasSubscription("A", "ns=3;s=\"数据块_1\".\"Static_1\"", SubCallback);
+            //1-取消单节点数据订阅  取消订阅 把Key值传入参数
+            //oPCUA.CancelSingleNodeIdDatasSubscription("A");
+
+
+            // 多个节点的订阅
+            MonitorNodeTags = new string[]
+            {
+                "ns=3;s=\"数据块_1\".\"Static_2\"",
+                "ns=3;s=\"数据块_1\".\"Static_3\"",
+                "ns=3;s=\"数据块_1\".\"Static_4\"",
+                "ns=3;s=\"数据块_1\".\"Static_5\"",
+                "ns=3;s=\"数据块_1\".\"Static_7\"[1000]",
+            };
+            oPCUA.BatchNodeIdDatasSubscription("B", MonitorNodeTags, SubCallback);
+
+        }
+        //1-单节点数据订阅的回调函数
+        private void SubCallback(string key, MonitoredItem monitoredItem, MonitoredItemNotificationEventArgs args)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<string, MonitoredItem, MonitoredItemNotificationEventArgs>(SubCallback), key, monitoredItem, args);
+                return;
+            }
+
+            if (key == "A")
+            {
+                // 如果有多个的订阅值都关联了当前的方法，可以通过key和monitoredItem来区分
+                MonitoredItemNotification notification = args.NotificationValue as MonitoredItemNotification;
+                if (notification != null)
+                {
+                    textBox1.Text = notification.Value.WrappedValue.Value.ToString();
+                }
+            }
+            else if (key == "B")
+            {
+                // 需要区分出来每个不同的节点信息
+                MonitoredItemNotification notification = args.NotificationValue as MonitoredItemNotification;
+                if (monitoredItem.StartNodeId.ToString() == MonitorNodeTags[0])
+                {
+                    textBox2.Text = notification.Value.WrappedValue.Value.ToString();
+                }
+                else if (monitoredItem.StartNodeId.ToString() == MonitorNodeTags[1])
+                {
+                    textBox3.Text = notification.Value.WrappedValue.Value.ToString();
+                }
+                else if (monitoredItem.StartNodeId.ToString() == MonitorNodeTags[2])
+                {
+                    textBox4.Text = notification.Value.WrappedValue.Value.ToString();
+                }
+                else if (monitoredItem.StartNodeId.ToString() == MonitorNodeTags[3])
+                {
+                    textBox5.Text = notification.Value.WrappedValue.Value.ToString();
+                }
+                else if (monitoredItem.StartNodeId.ToString() == MonitorNodeTags[4])
+                {
+                    textBox6.Text = notification.Value.WrappedValue.Value.ToString();
+                }
+                else if (monitoredItem.StartNodeId.ToString() == MonitorNodeTags[5])
+                {
+                    textBox7.Text = notification.Value.WrappedValue.Value.ToString();
+                }
+            }
+        }
+
+
+
+
+        private void loop1()
+        {
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    bool isconn = oPCUA.ConnectStatus;
+                    this.Invoke(new Action(() =>
+                    {
+                        if (isconn)
+                        {
+                            label1.BackColor = Color.Green;
+                        }
+                        else
+                        {
+                            label1.BackColor = Color.Red;
+                        }
+                        //textBox1.Text = a1.ToString();
+                        //textBox2.Text = a2.ToString();
+                        //textBox3.Text = a3.ToString();
+                        //textBox4.Text = a4.ToString();
+                        //textBox5.Text = a5.ToString();
+                        //textBox6.Text = a6.ToString();
+                        //textBox7.Text = a7.ToString();
+                        //textBox8.Text = a8.ToString();
+                        //textBox9.Text = a9.ToString();
+                    }));
+                    Thread.Sleep(1000);
+                }
+            });
+        }
+    }
+}
